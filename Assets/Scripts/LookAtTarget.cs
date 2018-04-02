f15d13975833c680ba86645d8b2f9d1560555d12
will <wenright0@gmail.com>
2018-04-01 22:39:35 -0400

    Initial commit:

---
+++ Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour {
+
+	private Transform target;
+
+	void Update ()
+	{
+		if (target == null)
+		{
+			return;
+		}
+
+		transform.LookAt(target);
+	}
+
+	public void SetTarget (Transform target)
+	{
+		this.target = target;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+	private LookAtTarget lookAtTarget;
+	private Rigidbody body;
+	private Vector3 mouseStartPos;
+	private float strength = 100.0f;
+
+	public override void OnStartLocalPlayer ()
+	{
+		base.OnStartLocalPlayer();
+
+		lookAtTarget = Camera.main.GetComponent<LookAtTarget> ();
+
+		lookAtTarget.SetTarget(transform);
+	}
+
+	public override void OnStartServer ()
+	{
+		base.OnStartServer();
+
+		body = GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+	{
+		if (isLocalPlayer)
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				mouseStartPos = Input.mousePosition;
+			}
+
+			if (Input.GetMouseButton(0))
+			{
+				// TODO hit direction grqphics
+			}
+
+			if (Input.GetMouseButtonUp(0))
+			{
+				Vector3 dir = (Quaternion.AngleAxis(90, Vector2.right) * (mouseStartPos - Input.mousePosition)).normalized;
+				CmdSwing(dir);
+			}
+		}
+	}
+
+	[Command]
+	private void CmdSwing (Vector3 dir)
+	{
+		body.AddForce(dir * strength);
+	}
+}
